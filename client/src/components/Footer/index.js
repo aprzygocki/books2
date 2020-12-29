ae5a29fc60e95768de59b990eb9f1a7b441e7233
aprzygocki <augustino.przygocki@yahoo.com>
2020-12-29 02:38:03 -0800

    copied previous version of googlebooks

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> Proudly built by <a href="https://augustino.us" target="_blank">Augustino Przygocki</a> using React.js
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
