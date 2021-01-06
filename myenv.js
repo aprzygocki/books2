de7f1bfd989f8c524a268d1ff513b7fcb7825612
aprzygocki <augustino.przygocki@yahoo.com>
2021-01-05 19:12:45 -0800

    yah know

--- myenv.js
+++ myenv.js
@@ -1,2 +1,4 @@
-module.exports = {MONGODB_URI: "mongodb+srv://aprzygocki:yfL3PMELil9vaPtp@cluster0.wge56.mongodb.net/googlebooks?retryWrites=true&w=majority"
+module.exports = {
+MONGODB_URI: "mongodb+srv://aprzygocki:yfL3PMELil9vaPtp@cluster0.wge56.mongodb.net/googlebooks?retryWrites=true&w=majority",
+NODE_OPTIONS: "--max_old_space_size=2560"
 }
