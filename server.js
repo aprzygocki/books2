281d8008b4118b70e961d12f4486986aaf6eed57
aprzygocki <augustino.przygocki@yahoo.com>
2021-01-05 18:29:06 -0800

    raw stuff

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv')
   .config();
 const express = require("express");
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,8 +14,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
+
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI,
@@ -22,6 +26,7 @@ mongoose.connect(
     useNewUrlParser: true
   }
 );
+
 // Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
